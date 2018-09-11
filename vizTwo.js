a80658a1cdbee8256d5f77cbd1619d827c0c38b4
Michael Blum <michael.jo.blum@gmail.com>
2018-09-10 20:32:24 -0400

    Viz Two Refined

--- vizTwo.js
+++ vizTwo.js
@@ -5,6 +5,7 @@ const svgTwo = d3.select("#vizTwo")
     .append("svg")
     .attr("width", width)
     .attr("height", height)
+    .attr("class","svgTwo")
 
 // var format = d3.format(",d");
 
@@ -19,7 +20,7 @@ d3.json("data.json", function(error, data) {
     if (error) throw error
 
     var depthScale = d3.scaleOrdinal()
-            .range("blue","purple","white","#pink")
+            .range("blue","green","pink")
 
     var nestedData = d3.nest()
             .key(function(d) { return d.country})
@@ -27,18 +28,25 @@ d3.json("data.json", function(error, data) {
             // total: d3.sum(v, function(d){ return d.estimate})};})
             .entries(data)
 
-            // console.log(nestedData)
+            console.log(nestedData)
 
     var packable = {id: "All Countries", values: nestedData}
     var packChart = d3.pack()
 
     packChart.size([width,height])
 
+    console.log(packable)
+
     var root = d3.hierarchy(packable, function(d){ return d.values})
                 .sum(d => d.estimate)
-                // .sum(function(d) {console.log(d.value.total)})
 
-    // console.log(packChart(root).descendants())
+    console.log(packChart(root).descendants())
+
+    var colors = ["green", "orange", "red", "blue"]
+
+    function randomColor() {
+        return colors[Math.floor(Math.random() * colors.length)]
+    }
 
     svgTwo
                 .append("g")
@@ -51,7 +59,16 @@ d3.json("data.json", function(error, data) {
                     .attr("r", function(d){ return d.r})
                     .attr("cx", function(d){ return d.x})
                     .attr("cy", function(d){ return d.y})
-                    .style("fill",d=> depthScale(d.depth))
+                    .style("fill", randomColor)
+                    .on("mouseenter", function () {
+                        d3.select(this).classed("hovering", true);
+                    })
+                    .on("mouseleave", function () {
+                        d3.select(this).classed("hovering", false);
+                    })
+                    .append("title")
+                    .text(function (d) {
+                        return d.data.country});
                     
                 
 
